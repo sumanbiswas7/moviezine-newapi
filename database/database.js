adfac65a31bd5f40268e5d671baa1a569006d6e0
suman <ronaldosuman97@gmail.com>
2022-06-19 11:20:03 +0530

    db err atmpt - 2

--- database/database.js
+++ database/database.js
@@ -6,7 +6,9 @@ const db = new Pool({
   user: process.env.DB_USER,
   password: process.env.USER_PASSWORD,
   database: process.env.DATABASE,
-  port: process.env.PORT,
+  port: 5432,
+  connectionString:
+    "postgres://mtzcqnjwragjen:6eec39dbd5deda0ca6bb0762fc948bbbc70ae8bec104e10865fbd05414cf18c1@ec2-3-224-8-189.compute-1.amazonaws.com:5432/d5k3tvrqtci2d8",
   ssl: {
     rejectUnauthorized: false,
   },
