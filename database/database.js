4f55b7a38b0384e4705936009ea73ca7660f8f7b
suman <ronaldosuman97@gmail.com>
2022-11-24 14:37:55 +0530

    update: env changes for render hosting

--- database/database.js
+++ database/database.js
@@ -4,11 +4,10 @@ require("dotenv").config();
 const db = new Pool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.USER_PASSWORD,
-  database: process.env.DATABASE,
-  port: 5432,
-  connectionString:
-    "postgres://mtzcqnjwragjen:6eec39dbd5deda0ca6bb0762fc948bbbc70ae8bec104e10865fbd05414cf18c1@ec2-3-224-8-189.compute-1.amazonaws.com:5432/d5k3tvrqtci2d8",
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  connectionString: process.env.DB_CONNECTION_STRING,
   ssl: {
     rejectUnauthorized: false,
   },
